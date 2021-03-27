b73a509fff2bc695cc9e60169eb59837ae2c0bda
drew <rostochek22@gmail.com>
2021-03-27 14:58:20 +0300

    fix remote connect to db #4

--- mongo/db_wrapper.go
+++ mongo/db_wrapper.go
@@ -15,8 +15,8 @@ type DbWrapper struct {
 
 func (d DbWrapper) Init() DbWrapper {
 	var cred options.Credential = options.Credential{Username: os.Getenv("MONGODB_USER"),
-		Password: os.Getenv("MONGODB_PWD"), AuthMechanism: "SCRAM-SHA-1"}
-	d.Client, _ = mongo.Connect(d.Ctx, options.Client().ApplyURI("mongodb://mongo:27017/nhl").SetAuth(cred))
+		Password: os.Getenv("MONGODB_PWD")}
+	d.Client, _ = mongo.Connect(d.Ctx, options.Client().ApplyURI("mongodb://mongo:27017").SetAuth(cred))
 	d.Collection = d.Client.Database("nhl").Collection("players")
 	return DbWrapper{Client: d.Client, Ctx: d.Ctx, Collection: d.Collection}
 }
